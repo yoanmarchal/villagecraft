d70caa4d910f7192abbf88b263ac6fc686f4f0e5
Marchal yoan <marchalyoan@gmail.com>
2026-08-16 10:53:04 +0200

    use native tsconfig path resolution & update vite plugin react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import path from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()] // Gère automatiquement tous tes alias TypeScript ! ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [react()],
 });
